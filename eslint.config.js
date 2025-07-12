c04edf7371c08d099730ab8618b324879ccf4625
ThomasFixe <dietrich.thomams0703@gmail.com>
2025-07-12 21:11:29 +0200

    fix: config eslint fichier de test

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,20 @@
 import expoFlat from "eslint-config-expo/flat.js";
 
-export default expoFlat;
+export default [
+  ...expoFlat,
+  {
+    files: ["**/__tests__/**/*.js", "**/*.test.js", "**/*.spec.js"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        beforeAll: "readonly",
+        beforeEach: "readonly",
+        afterAll: "readonly",
+        afterEach: "readonly",
+      },
+    },
+  },
+];
